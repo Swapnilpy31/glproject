f9607bfdf58d99caf8ad046574e7da65ed767d90
Swapnil <109898004+Swapnilpy31@users.noreply.github.com>
2026-02-24 20:16:45 +0530

    remove spce

--- api-gateway/src/main/java/com/gl/apigateway/config/DataInitializer.java
+++ api-gateway/src/main/java/com/gl/apigateway/config/DataInitializer.java
@@ -15,7 +15,6 @@ import java.util.Set;
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
-
     @Override
     public void run(String... args) throws Exception {
         // Check if admin verification data exists
